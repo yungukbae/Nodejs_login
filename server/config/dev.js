4a4cba56803463bc89b1f2484d2b8d5cf4e01f26
yungukbae <yungukbae@gmail.com>
2021-04-02 13:42:18 +0900

    change

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,3 +1,3 @@
 module.exports ={
-    mongoURI:"mongodb+srv://yungukbae:qwe123@nodejsreactbasic.yyhbp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    mongoURI:"mongodb+srv://yungukbae:@nodejsreactbasic.yyhbp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 }
